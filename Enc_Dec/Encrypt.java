7207972da7b4e98932a6121c8447fd4c4622d028
Lakshminarayana Asula <Lakshminarayana.Asula@XEBIAINDIA.LOCAL>
2022-11-02 17:09:04 +0530

    Code added

---
+++ Enc_Dec/Encrypt.java
@@ -0,0 +1,27 @@
+package Enc_Dec;
+
+public class Encrypt {
+
+//    function encrypt(objectToEncrypt, password) {
+//
+//        if (password != null) {
+//            var encryptedParams = CryptoJS.AES.encrypt(JSON.stringify(objectToEncrypt), password);
+//            encryptedParams = CryptoJS.enc.Utf8.parse(encryptedParams);
+//            encryptedParams = CryptoJS.enc.Base64.stringify(encryptedParams);
+//            var end = new Date();
+//            var timetaken = (end - start) / 1000;
+//            MFP.Logger.info("encryptResponse Exit timetaken: " + timetaken.toString());
+//            return {
+//                    "encryptedString": encryptedParams
+//        };
+//        } else {
+//            return objectToEncrypt;
+//        }
+//    }
+}
+
+
+//Note:
+//        -----
+//        password is like secret key or AccessValue
+//        Ex: 646f04fe-f818-4a03-a321-124e5b9c9700
