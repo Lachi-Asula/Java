6af098ffd9469a12c6eb77583daaf76f3d714dd8
Lakshminarayana Asula <Lakshminarayana.Asula@XEBIAINDIA.LOCAL>
2022-11-11 11:05:16 +0530

    Reverse order array using Collections.reverse()

--- Java8Feature/src/Main.java
+++ Java8Feature/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        Integer[] x = {1,3,5,6};
+        int y = 10;
+        Integer z= y;
+        List<Integer> i = Arrays.asList(x);
+        Collections.reverse(i);
+        System.out.println(x);
         System.out.println("Hello world!");
     }
 }
